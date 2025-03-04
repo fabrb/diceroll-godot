4176d4d3432138f210e2c7372115c246dc85ac13
fabrb <faiorb@outlook.com>
2025-03-04 11:37:16 -0300

    upgrade to 4.4

--- props/dieValueRender.gd
+++ props/dieValueRender.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 func _ready() -> void:
+	print(1)
 	for die in get_tree().get_nodes_in_group("die"):
 		if (die == self.get_parent().get_parent()):
 			for value: Label3D in get_tree().get_nodes_in_group("dieValue"):
